16903e25fe2c7b273fd763e68c3113a628bfa07a
dfunani <dfunani@live.co.za>
2024-01-11 20:21:47 +0200

    pushing added line tests

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -12,7 +12,7 @@ color = (255, 0, 0)
 
 
 def test_line_init():
-    color = (255, 0, 0)
+    """test_line_class"""
     line = Line((x0, y0), (x1, y1), color)
     assert line.x0 == 0
     assert line.x1 == 1
@@ -21,22 +21,41 @@ def test_line_init():
 
 
 def test_line_init_error():
+    """test line init when error raised - Passed str not tuple"""
     with raises(TypeError):
-        line = Line("(x0, y0)", (x1, y1), color)
+        Line("(x0, y0)", (x1, y1), color)
+
 
 def test_line_init_error1():
+    """test line init when error raised - passed tuple with
+    a str element
+    """
     with raises(ValueError):
-        line = Line((x0, y0), (x1, y1), (20,"","color",))
+        Line(
+            (x0, y0),
+            (x1, y1),
+            (
+                20,
+                "",
+                "color",
+            ),
+        )
+
 
 def test_str():
+    """test str rep of the line class"""
     line = Line((x0, y0), (x1, y1), color)
     assert str(line) == "Line from (0, 2) to (1, 3) with color (255, 0, 0)"
 
+
 def test_swap_if_steep():
+    """test steep check func"""
     line = Line((x0, y0), (x1, y1), color)
     assert not line.swap_if_steep()
 
+
 def test_get_y_value():
+    """test getting y value"""
     line = Line((x0, y0), (x1, y1), color)
     y = line.get_y_value(line.x0)
     assert y == 2
